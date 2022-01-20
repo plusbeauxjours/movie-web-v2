3ed0099a2369793e7bf3f25b6b1f6350580dd4c2
plusbeauxjours <plusbeauxjours@gmail.com>
2022-01-21 00:34:20 +0700

    fix: typo

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
+import { theme } from "./theme";
 import App from "./App";
 import { GlobalStyle } from "./globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
